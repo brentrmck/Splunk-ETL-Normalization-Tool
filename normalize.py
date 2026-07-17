efeb21cb5e91f7dddd5e0d0cc3b29b0ff6c4c460
Brent Mckinney <brentrmck@gmail.com>
2026-07-17 08:27:13 -0400

    Updated loop
    Updated to make more accurate. Loop iterates over the passed in file, and for every line in that file, append that line as a dictionary into the events list.

--- normalize.py
+++ normalize.py
@@ -3,6 +3,6 @@ import json
 def load_events(jsonl_path):
     with open(jsonl_path) as file:
         events = []
-        for event in file:
-            events.append(json.loads(event))
+        for line in file:
+            events.append(json.loads(line))
     return events
