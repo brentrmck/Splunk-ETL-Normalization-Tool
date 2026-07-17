aaaf5f1784aa0ec25813a698d3dcf53f2e837d8d
Brent Mckinney <brentrmck@gmail.com>
2026-07-17 17:03:11 -0400

    Update normalize.py
    Added try / except for error handling. Also added parsed + failed counts to keep track of successful loads of json objects to the events list.

--- normalize.py
+++ normalize.py
@@ -3,6 +3,15 @@ import json
 def load_events(jsonl_path):
     with open(jsonl_path) as file:
         events = []
+        parsed_count = 0
+        failed_count = 0
         for line in file:
-            events.append(json.loads(line))
+            try:
+                events.append(json.loads(line))
+                parsed_count += 1
+            except json.JSONDecodeError:
+                failed_count += 1
+        print("parsed: " + str(parsed_count) + ", failed: " + str(failed_count))
     return events
+
+print(load_events("data/sample_events.jsonl"))
