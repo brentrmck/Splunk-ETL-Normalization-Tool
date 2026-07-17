4a268bf8a8b4c4fb2f210e13b8a403abfdeef1e3
Brent Mckinney <brentrmck@gmail.com>
2026-07-17 07:17:22 -0400

    Added json loader

--- normalize.py
+++ normalize.py
@@ -1,2 +1,8 @@
-# Project Code Here
-print("hello world")
+import json
+
+def load_events(jsonl_path):
+    with open(jsonl_path) as file:
+        events = []
+        for event in file:
+            events.append(json.loads(event))
+    return events
